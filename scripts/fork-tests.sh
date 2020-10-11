0aa55a3b40e1cddf11fb8cb2326ac05bed3e0bca
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-10-11 11:41:27 -0400

    Test for upstream package usage. (#142)

--- scripts/fork-tests.sh
+++ scripts/fork-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# These are a few simple test to make sure the repository directory is as we expect.
-# This is mostly used to prevent errors when merging upstream changes.
+# These are a few simple tests to make sure the repository directory is as we
+# expect. This is mostly used to prevent errors when merging upstream changes.
 # This should be ran from the root of the repository.
 
 failed=""
@@ -15,6 +15,12 @@ fi
 # Make sure that we pulled an actual release and not a dev version
 if grep -q "DEV" ./common/hugo/version_current.go;then
 	echo "Error: It looks like a dev version of Hugo was pulled."
+	failed="true"
+fi
+
+# Make sure we're not using packages from github.com/gohugoio/hugo
+if grep -q "github.com\/gohugoio\/hugo" ./go.mod;then
+	echo "Error: It looks like a package from github.com/gohugoio/hugo is being used."
 	failed="true"
 fi
 
