8f120082a3bf7f5abecdc247b44bf61fd38ec00e
Michael Ibragimchayev <michael@chayev.com>
2020-12-31 12:58:41 -0700

    Open dev for next release. (#230)

--- src/common/hugo/version-gotham.go
+++ src/common/hugo/version-gotham.go
@@ -7,7 +7,7 @@ package hugo
 // There's an include base version of Hugo in a different file.
 var GothamVersion = SemVerVersion{
 	Major:  0,
-	Minor:  11,
+	Minor:  12,
 	Patch:  0,
-	Suffix: "",
+	Suffix: "dev",
 }
