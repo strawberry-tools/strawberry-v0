f5df331c207c108a839ce8f0ea1ddff29d5b8432
Michael Ibragimchayev <michael.ibragimchayev@gmail.com>
2021-01-09 19:40:28 -0700

    wip

--- src/hugolib/aasa_test.go
+++ src/hugolib/aasa_test.go
@@ -52,9 +52,27 @@ func TestAASAOutput(t *testing.T) {
 			true,
 		},
 		{
+			"ABCDE12345",
+			"",
+			1,
+			false,
+		},
+		{
+			"",
+			"com.gothamhq.ios",
+			1,
+			false,
+		},
+		{
 			"",
 			"",
 			1,
+			false,
+		},
+		{
+			"",
+			"",
+			0,
 			false,
 		},
 	}
@@ -66,6 +84,7 @@ func TestAASAOutput(t *testing.T) {
 		cfg.Set("baseURL", "http://gotham/test/")
 		cfg.Set("aasaPrefix", tc.prefix)
 		cfg.Set("aasaBundle", tc.bundle)
+		cfg.Set("aasaVersion", tc.version)
 
 		depsCfg := deps.DepsCfg{Fs: fs, Cfg: cfg}
 
@@ -78,6 +97,14 @@ func TestAASAOutput(t *testing.T) {
 
 			th.assertFileNotExist(outputAASA)
 			return
+		}
+
+		if cfg.Get("aasaVersion") == 2 {
+			th.assertFileContent(outputAASA, "\"components\":")
+		} else if cfg.Get("aasaVersion") == 1 {
+			th.assertFileContent(outputAASA, "\"apps\": []")
+		} else {
+			t.Error("Error: Not a valid AASA version.")
 		}
 
 		th.assertFileContent(outputAASA,
