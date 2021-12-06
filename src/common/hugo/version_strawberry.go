0a5fdfbeb797441f3273502d7a63d8b6414070e4
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-12-05 21:25:47 -0500

    Release: Strawberry v0.20.1
    Fixing the version output when running `strawberry serve`. Before this
    fix, it was outputting the Hugo version instead of the Strawberry
    version.
    
    Strawberry v0.20.1 (compatible with Hugo v0.89.4/extended)

--- src/common/hugo/version_strawberry.go
+++ src/common/hugo/version_strawberry.go
@@ -7,7 +7,7 @@ package hugo
 // There's an include base version of Hugo in a different file.
 var StrawberryVersion = SemVerVersion{
 	Major:  0,
-	Minor:  21,
-	Patch:  0,
-	Suffix: "dev",
+	Minor:  20,
+	Patch:  1,
+	Suffix: "",
 }
