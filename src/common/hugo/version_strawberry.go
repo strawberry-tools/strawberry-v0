148a5042449c9cc16527c52052f9da9479a66e91
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-12-11 20:48:03 -0500

    Open dev for release

--- src/common/hugo/version_strawberry.go
+++ src/common/hugo/version_strawberry.go
@@ -7,7 +7,7 @@ package hugo
 // There's an include base version of Hugo in a different file.
 var StrawberryVersion = SemVerVersion{
 	Major:  0,
-	Minor:  21,
+	Minor:  22,
 	Patch:  0,
-	Suffix: "",
+	Suffix: "dev",
 }
